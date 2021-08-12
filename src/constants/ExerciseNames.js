6a4f318bed64ee75eb10e4d032a7b8297026fec1
demaiodev <fml1041@gmail.com>
2021-08-12 13:33:09 -0400

    add exercise names constants

---
+++ src/constants/ExerciseNames.js
@@ -0,0 +1,9 @@
+const ExerciseNames = {
+  squat: "Squat",
+  overheadPress: "Overhead Press",
+  barbellRow: "Barbell Row",
+  deadlift: "Deadlift",
+  benchPress: "Bench Press",
+};
+
+export default ExerciseNames;
