797c75212b73f4873f8f32dfc040706873e88d2c
Tyler Langties <tylerlangties@gmail.com>
2021-08-12 11:17:13 -0400

    add generic home and workout views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,67 +2,57 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>SkrongLifts</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <!-- could eventually loop over the next 3 workouts here -->
+      <Card :source="nextWorkout" />
+      <!-- <ion-button
+        @click="$router.push('/workout')"
+        size="large"
+        color="danger"
+        class="ml-1 mr-1 pt-2"
+        expand="block"
+        >Start Workout</ion-button
+      > -->
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  // IonButton,
+} from "@ionic/vue";
+import Card from "../components/Card.vue";
+import { getNextWorkout } from "@/services/index.js";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
+    Card,
+    // IonButton,
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
+  setup() {
+    const nextWorkout = getNextWorkout();
+    return { nextWorkout };
+  },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
---
+++ src/views/Workout.vue
@@ -0,0 +1,102 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-text slot="end"><h6 class="pr-2 m-0">Workout A</h6></ion-text>
+        <ion-title>Back</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-text>
+              <h6 class="m-0">Squat</h6>
+            </ion-text>
+          </ion-col>
+          <ion-col class="exercise__header">
+            <ion-button @click="$router.push('/edit-exercise')"
+              >5x5 45lb <ion-icon :icon="arrowForwardOutline"></ion-icon
+            ></ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col class="center">
+            <SetButton />
+          </ion-col>
+          <ion-col class="center"> 
+            <SetButton />
+          </ion-col>
+          <ion-col class="center">
+            <SetButton />
+          </ion-col>
+          <ion-col class="center">
+            <SetButton />
+          </ion-col>
+          <ion-col class="center">
+            <SetButton />
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonGrid,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from "@ionic/vue";
+import SetButton from '../components/SetButton.vue'
+import { arrowForwardOutline } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    SetButton,
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonButton,
+    IonRow,
+    IonGrid,
+    IonCol,
+    IonText,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  },
+  setup() {
+    return { arrowForwardOutline };
+  },
+});
+</script>
+
+<style>
+.exercise__header {
+  display: flex;
+  justify-content: flex-end;
+}
+.center{
+  display: flex;
+  justify-content: center;
+}
+</style>
