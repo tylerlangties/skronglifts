7d6ca1825ee86e8aecdca52d9af0a10bbcde52d7
Tyler Langties <tylerlangties@gmail.com>
2021-08-12 11:14:18 -0400

    add basic card component

---
+++ src/components/Card.vue
@@ -0,0 +1,57 @@
+<template>
+  <ion-card class="Card" @click="$router.push('/workout')">
+    <ion-item>
+      <ion-card-subtitle slot="start"
+        >Workout {{ source.workoutType }}</ion-card-subtitle
+      >
+      <ion-card-subtitle slot="end">{{ source.date }}</ion-card-subtitle>
+    </ion-item>
+
+    <ion-item
+      v-for="(exercise, key) in source.exercises"
+      :key="key"
+      class="ion-activated"
+    >
+      <ion-label slot="start">{{ exercise.label }}</ion-label>
+      <ion-label class="right" slot="end"
+        >{{ `${exercise.setCount}x${exercise.repCount}` }}
+        {{ exercise.weight }}lb</ion-label
+      >
+    </ion-item>
+  </ion-card>
+</template>
+
+<script>
+import { IonCard, IonItem, IonLabel, IonCardSubtitle } from "@ionic/vue";
+import { warning } from "ionicons/icons";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    IonCard,
+    IonItem,
+    IonLabel,
+    IonCardSubtitle,
+  },
+  props: {
+    source: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return { warning };
+  },
+});
+</script>
+
+<style>
+.right {
+  text-align: right;
+}
+
+.Card {
+  border-left: 5px solid red;
+}
+</style>
+
