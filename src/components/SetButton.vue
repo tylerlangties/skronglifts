6c1ff9cfd13ee4bf8aed69e249bd8aee496ade21
Tyler Langties <tylerlangties@gmail.com>
2021-08-12 11:14:36 -0400

    add generic setbutton component

---
+++ src/components/SetButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-button @click="onSelect()" :color="color" shape="round">{{ repCount }}</ion-button>
+</template>
+
+<script lang="ts">
+import { IonButton } from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    IonButton,
+  },
+  setup() {
+    const color = ref("primary");
+    const complete = ref(false);
+    const repCount = ref(5);
+
+    const onSelect = () => {
+      if (complete.value === false) {
+        complete.value = true;
+        color.value = "danger";
+      } else if (repCount.value === 0) {
+        complete.value = false;
+        repCount.value = 5;
+        color.value="primary"
+      } else {
+        repCount.value--;
+      }
+    };
+    return { complete, repCount, color, onSelect };
+  },
+});
+</script>
