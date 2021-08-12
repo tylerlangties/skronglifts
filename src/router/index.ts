230273dc2d67d2ad750a6163f7472a6ccaac257a
Tyler Langties <tylerlangties@gmail.com>
2021-08-12 11:14:59 -0400

    add workout and edit exercise views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
+import Workout from '../views/Workout.vue'
+import EditExercise from '../views/EditExercise.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +13,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/workout',
+    name: 'Workout',
+    component: Workout
+  },
+  {
+  path: '/edit-exercise',
+  name: 'EditExercise',
+  component: EditExercise
   }
 ]
 
